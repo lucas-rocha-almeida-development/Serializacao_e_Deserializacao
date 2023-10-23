a8ab963eeeb9f4f5b9c56ebf7264c0a67d7c4b13
Lucas Almeida <lucalmr12@gmail.com>
2023-10-23 10:08:40 -0300

    Adicionar arquivos de projeto.

---
+++ Serializacao_e_Deserializacao/Cachorro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Serializacao_e_Deserializacao
+{
+    [DataContract]
+    internal class Cachorro
+    {
+        [DataMember] //membros da classe
+        public string Nome {  get; set; }
+        [DataMember]
+        public string Raca { get; set; }
+        [DataMember]
+
+        private string Cor;
+
+        public Cachorro(string pnome, string praca, string pcor)
+        {
+            this.Nome = pnome;
+            this.Raca = praca;
+            this.Cor = pcor;
+        }
+
+        //construtor padrão
+        public Cachorro()
+        {
+
+        }
+    }
+}
---
+++ Serializacao_e_Deserializacao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Runtime.Serialization;
+
+
+namespace Serializacao_e_Deserializacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //objeto da class que será serealizada
+            Cachorro meuCachorro = new Cachorro("Lobinho","Husky-Siberiano","Branco&Preto");
+            Cachorro meuCachorroDesserializado;
+
+            DataContractSerializer serializador = new DataContractSerializer(typeof(Cachorro));
+            //Serealização 
+            XmlWriterSettings xmlConfig = new XmlWriterSettings {Indent=true };//mantenha o codigo organizado
+            StringBuilder construtorDeString = new StringBuilder();//escreva em formato de string
+            XmlWriter escrevaNoFormatoXml = XmlWriter.Create(construtorDeString, xmlConfig);
+            serializador.WriteObject(escrevaNoFormatoXml, meuCachorro);//escreva um objeto cachorro e transforme em XML
+            escrevaNoFormatoXml.Flush();//FINALIZAÇÃO DA ESCRITA XML ( CONVERSÃO ENCERRADA)
+            string objetoserializadoSrt = construtorDeString.ToString();
+
+            //salvando o conteudo do objeto em um xml
+            string caminhodomeuArquivoXml = "Cachorro.xml";
+            FileStream meuArquivoXml = File.Create(caminhodomeuArquivoXml);
+            meuArquivoXml.Close();
+            File.WriteAllText(caminhodomeuArquivoXml, objetoserializadoSrt);
+
+            //Desserializar //PEGAR O ARQUIVO XML E TRANSFORMA EM UM OBJETO (INVERSO)
+
+            string conteudoDoObjSerializado = File.ReadAllText(caminhodomeuArquivoXml);//
+            StringReader LeitorDeString = new StringReader(conteudoDoObjSerializado);//leia o conteudo do arquivo.xml
+            XmlReader LeitorDeXml = XmlReader.Create(LeitorDeString);
+            meuCachorroDesserializado = (Cachorro)serializador.ReadObject(LeitorDeXml);
+
+            escrevaNoFormatoXml.Close();
+            LeitorDeXml.Close();
+        }
+    }
+}
